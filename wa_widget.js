1ec2c1613a48929125ca3ef610e98a993323b099
tayfunbalci <tayfunbalci@outlook.com>
2024-07-05 12:32:03 +0300

    widget animation and send message

---
+++ wa_widget.js
@@ -0,0 +1,31 @@
+(function () {
+
+    const btn = document.querySelector('.wa__btn_popup');
+    btn.addEventListener('click', function () {
+        if (btn.classList.contains("wa__active")) {
+            btn.classList.remove("wa__active");
+            $('.wa__popup').fadeOut("slow");
+        } else {
+            btn.classList.add("wa__active");
+            $('.wa__popup').fadeIn("slow");
+        }
+    })
+
+    const phoneNumber = "908503056191"; // Telefon numarası
+    let message = ""; // Mesaj
+    let whatsappUrl = `https://api.whatsapp.com/send?phone=${phoneNumber}&text=${encodeURIComponent(message)}`;
+
+    const messageInput = document.getElementById("messageInput");
+    messageInput.addEventListener("input", function () {
+        message = messageInput.value; // Input alanının değerini `message` değişkenine atayın
+        whatsappUrl = `https://api.whatsapp.com/send?phone=${phoneNumber}&text=${encodeURIComponent(message)}`;
+    });
+
+
+    const whatsappLink = document.querySelector(".wa__popup_send_btn");
+    whatsappLink.addEventListener("click", function (event) {
+        event.preventDefault(); // Varsayılan bağlantı davranışını önleyin
+        window.open(whatsappUrl, "_blank"); // WhatsApp bağlantısını yeni bir sekmede açın
+    });
+
+})();
